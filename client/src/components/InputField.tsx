16c18adde0548d4a332d96935c283191ebe69287
Nguyễn Trần Hồng Trí <tricdt@gmail.com>
2022-09-24 10:01:55 +0700

    create post and update cache apollo after create

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -5,20 +5,26 @@ import {
    FormErrorMessage,
    FormLabel,
    Input,
+   Textarea,
 } from "@chakra-ui/react";
 interface InputFieldProps {
    name: string;
    label: string;
    placeholder: string;
    type: string;
+   textarea?: boolean;
 }
 
-const InputField = (props: InputFieldProps) => {
+const InputField = ({ textarea, ...props }: InputFieldProps) => {
    const [field, { error }] = useField(props);
    return (
       <FormControl isInvalid={!!error}>
          <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-         <Input {...field} id={field.name} {...props} />
+         {textarea ? (
+            <Textarea {...field} id={field.name} {...props} />
+         ) : (
+            <Input {...field} id={field.name} {...props} />
+         )}
          {error && <FormErrorMessage>{error}</FormErrorMessage>}
       </FormControl>
    );
