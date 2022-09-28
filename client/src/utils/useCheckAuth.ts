de71281fe4abd0335f02d8233ee095409e6c4518
Nguyễn Trần Hồng Trí <tricdt@gmail.com>
2022-09-28 10:29:10 +0700

    deletePost and update cache apollo client

--- client/src/utils/useCheckAuth.ts
+++ client/src/utils/useCheckAuth.ts
@@ -11,7 +11,7 @@ export const useCheckAuth = () => {
             data?.me &&
             (router.route === "/login" ||
                router.route === "/register" ||
-               router.route === "forgot-password" ||
+               router.route === "/forgot-password" ||
                router.route === "/change-password")
          ) {
             router.replace("/");
